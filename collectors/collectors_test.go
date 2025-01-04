3f9915dfdb82d18000c43380c9da3778e7e8e286
treydock <tdockendorf@osc.edu>
2025-01-04 11:49:43 -0500

    Associate switch rate with port (#31)
    * Replaces `infiniband_switch_rate_bytes_per_second` with `infiniband_switch_port_rate_bytes_per_second`
    * Replaces `infiniband_switch_raw_rate_bytes_per_second` with `infiniband_switch_port_raw_rate_bytes_per_second`

--- collectors/collectors_test.go
+++ collectors/collectors_test.go
@@ -30,16 +30,16 @@ var (
 	mockedStdout     string
 	_, cancel        = context.WithTimeout(context.Background(), 5*time.Second)
 	switchDevices    = []InfinibandDevice{
-		{Type: "SW", LID: "2052", GUID: "0x506b4b03005c2740", Rate: (25 * 4 * 125000000), RawRate: 1.2890625e+10, Name: "ib-i4l1s01",
+		{Type: "SW", LID: "2052", GUID: "0x506b4b03005c2740", Name: "ib-i4l1s01",
 			Uplinks: map[string]InfinibandUplink{
-				"35": {Type: "CA", LID: "1432", PortNumber: "1", GUID: "0x506b4b0300cc02a6", Name: "p0001 HCA-1"},
+				"35": {Type: "CA", LID: "1432", PortNumber: "1", GUID: "0x506b4b0300cc02a6", Name: "p0001 HCA-1", Rate: (25 * 4 * 125000000), RawRate: 1.2890625e+10},
 			},
 		},
-		{Type: "SW", LID: "1719", GUID: "0x7cfe9003009ce5b0", Rate: (25 * 4 * 125000000), RawRate: 1.2890625e+10, Name: "ib-i1l1s01",
+		{Type: "SW", LID: "1719", GUID: "0x7cfe9003009ce5b0", Name: "ib-i1l1s01",
 			Uplinks: map[string]InfinibandUplink{
-				"1":  {Type: "SW", LID: "1516", PortNumber: "1", GUID: "0x7cfe900300b07320", Name: "ib-i1l2s01"},
-				"10": {Type: "CA", LID: "134", PortNumber: "1", GUID: "0x7cfe9003003b4bde", Name: "o0001 HCA-1"},
-				"11": {Type: "CA", LID: "133", PortNumber: "1", GUID: "0x7cfe9003003b4b96", Name: "o0002 HCA-1"},
+				"1":  {Type: "SW", LID: "1516", PortNumber: "1", GUID: "0x7cfe900300b07320", Name: "ib-i1l2s01", Rate: (25 * 4 * 125000000), RawRate: 1.2890625e+10},
+				"10": {Type: "CA", LID: "134", PortNumber: "1", GUID: "0x7cfe9003003b4bde", Name: "o0001 HCA-1", Rate: (25 * 4 * 125000000), RawRate: 1.2890625e+10},
+				"11": {Type: "CA", LID: "133", PortNumber: "1", GUID: "0x7cfe9003003b4b96", Name: "o0002 HCA-1", Rate: (25 * 4 * 125000000), RawRate: 1.2890625e+10},
 			},
 		},
 	}
